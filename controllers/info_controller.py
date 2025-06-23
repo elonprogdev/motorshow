38e2044e6c66bf9c4b06d3913465fb36436e1bdd
elonprogdev <mordoki2021@gmail.com>
2025-06-23 20:58:11 +0300

    implementing controllers in project

---
+++ controllers/info_controller.py
@@ -0,0 +1,17 @@
+from flask import render_template, session, request, redirect, url_for, send_file
+from models.car import Car 
+import random
+
+# @app.route('/')
+def home():
+    all_cars = Car.get_all_cars_data()
+
+    # случайные 4 без повторов
+    random_cars = random.sample(all_cars, 4)
+
+    # сортировка по ID (предположим, ID — это первый элемент)
+    sorted_cars = sorted(random_cars, key=lambda x: x.id)
+
+    return render_template('home.html',
+                           cars_models=sorted_cars,
+                           css_path=url_for('static', filename='css/home.css'))
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 from flask import Flask
-from flask import render_template, session, request, redirect, url_for, send_file
-from models.car import Car 
-import sqlite3
-import random
+from controllers import info_controller
 
 app = Flask("motorshow")
 
-@app.route('/')
-def index():
-    all_cars = Car.get_all_cars_data()
-
-    # случайные 4 без повторов
-    random_cars = random.sample(all_cars, 4)
-
-    # сортировка по ID (предположим, ID — это первый элемент)
-    sorted_cars = sorted(random_cars, key=lambda x: x.id)
-
-    return render_template('index.html',
-                           cars_models=sorted_cars,
-                           css_path=url_for('static', filename='css/home.css'))
 
 
-                
-        
+app.add_url_rule("/", "home", info_controller.home)       
 
 
 
